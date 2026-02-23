da1019805e77c64bc023324e24d0918e5252808f
moubarak1ezzyani <moubarak.ezzyani@gmail.com>
2026-02-23 07:54:17 +0000

    refactor(main): clean up test routes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,7 @@
 # files imports
 from api.database import my_Base, engine
-from api.schemas import password, TokenData
-from core.config import SECRET_KEY, ALGO, TOKEN_EXPIRE
-
 # Libs
-from fastapi import FastAPI, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
-from passlib.context import CryptContext
-from pydantic import BaseModel
-import jwt
-from typing import Optional
-from datetime import time, timedelta, datetime, UTC, minutes
-
-
-    
-
+from fastapi import FastAPI
 
 
 # ------------------------------------
