730bdfbc25ca98a8d8ca3c38cc37f6d8ea12bc5c
moubarak1ezzyani <moubarak.ezzyani@gmail.com>
2026-02-23 08:00:38 +0000

    feat(config): add security configuration variables to environment loader
    - Added `SECRET_KEY`, `ALGO`, and `TOKEN_EXPIRE` variables to the configuration file.
    - Configured the app to load these security parameters securely using `os.getenv()`.
    - Prepared the environment to handle JWT token generation and validation.

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -10,3 +10,9 @@ DB_HOST=os.getenv("DB_HOST_env")
 DB_PORT=os.getenv("DB_PORT_env")
 DB_NAME=os.getenv("DB_NAME_env")
 DB_URL=f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+
+# --- security 
+SECRET_KEY=os.getenv("SECRET_KEY_env")
+ALGO=os.getenv("ALGO_env")
+TOKEN_EXPIRE=os.getenv("TOKEN_EXPIRE_env")
